68c7d293a948cde85e9a28ffc155a541ce1c1004
Mono <boyhuanggordon@gmail.com>
2025-10-21 18:15:54 +0800

    2025/10/22

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from mlp import MLP
 
 data = np.loadtxt("train4dAll.txt")
 X = data[:,:-1]
-y = data[:, -1]
+y = data[:, -1].reshape(-1, 1)
 
-mlp  = MLP()
+mlp = MLP(hidden_layer_sizes=[8,4,2], learning_rate=0.01, epochs=100)
 mlp.fit(X, y)
 
 for sample in range(len(X)):
-	print(mlp.predict(X[sample]), y[sample])
+	print(mlp.predict(X[sample]), y[sample, 0])
--- mlp.py
+++ mlp.py
@@ -2,18 +2,34 @@ import numpy as np
 
 
 class MLP:
-	def __init__(self, hidden_layer_sizes=(20,12), learning_rate=0.001, epochs=100):
+	def __init__(self, hidden_layer_sizes=[8,4], learning_rate=0.001, epochs=100):
 		self.hidden_layer_sizes = hidden_layer_sizes
 		self.learning_rate = learning_rate
 		self.epochs = epochs
 
+	def _standardize(self, X):
+		self.mean = np.mean(X, axis=0)
+		self.std = np.std(X, axis=0)
+
+	def _standardize_transform(self, X):
+		return (X - self.mean) / self.std
+
+	def _minmax(self, min, max):
+		self.y_min = min
+		self.y_max = max
+
+	def _minmax_transform(self, y):
+		return (y - self.y_min) / (self.y_max - self.y_min)
+
+	def _minmax_transform_inverse(self, y):
+		return y * (self.y_max - self.y_min) + self.y_min
+
 	def _initialize_weights(self, n_features, n_outputs):
-		layer_sizes = [n_features] + list(self.hidden_layer_sizes) + [n_outputs]
+		layer_sizes = [n_features, *self.hidden_layer_sizes, n_outputs]
 		self.weights = []
 
 		for i in range(len(layer_sizes) - 1):
-			W = np.random.randn(layer_sizes[i + 1], layer_sizes[i] + 1) * np.sqrt(2 / layer_sizes[i])
-			self.weights.append(W)
+			self.weights.append(np.random.randn(layer_sizes[i + 1], layer_sizes[i] + 1))
 
 	def _activation(self, v):
 		return np.maximum(0, v)
@@ -36,34 +52,41 @@ class MLP:
 	def _backward(self, activations, y):
 		grads_W = [None] * len(self.weights)
 
-		delta = (y - activations[-1]) * self._activation_derive(activations[-1])
-		activation_with_bias = np.insert(activations[-2], 0, -1).reshape(-1, 1)
-		grads_W[-1] = np.outer(delta, activation_with_bias)
+		delta = (y - activations[-1])
+		grads_W[-1] = np.outer(delta.reshape(-1), np.insert(activations[-2], 0, -1))
 
 		for i in reversed(range(len(self.weights) - 1)):
-			delta = self._activation_derive(activations[i + 1]) * (self.weights[i + 1].T[1:] @ delta)
-			activation_with_bias = np.insert(activations[i], 0, -1).reshape(-1, 1)
-			grads_W[i] = np.outer(delta, activation_with_bias)
+			delta = self._activation_derive(activations[i + 1]) * (self.weights[i + 1][:, 1:].T @ delta)
+			grads_W[i] = np.outer(delta.reshape(-1), np.insert(activations[i], 0, -1))
 
 		return grads_W
 
 	def fit(self, X, y):
 		X = np.asarray(X)
-		y = np.asarray(y).reshape(-1, 1)
+		y = np.asarray(y)
+
+		self._standardize(X)
+		X = self._standardize_transform(X)
+		self._minmax(-40, 40)
+		y = self._minmax_transform(y)
 
 		n_samples, n_features = X.shape
 		n_outputs = y.shape[1]
 		self._initialize_weights(n_features, n_outputs)
 
 		for epoch in range(self.epochs):
-			for sample_index in range(len(X)):
+			for sample_index in range(n_samples):
 				activations = self._forward(X[sample_index])
 				grads_W = self._backward(activations, y[sample_index])
 
 				for i in range(len(self.weights)):
 					self.weights[i] += self.learning_rate * grads_W[i]
 
+	def get_weights(self):
+		return self.weights
+
 	def predict(self, x):
 		x = np.asarray(x)
-		activations = self._forward(x)
-		return activations[-1][0, 0]
+		self._standardize_transform(x)
+		prediction = self._forward(x)[-1][0, 0]
+		return self._minmax_transform_inverse(prediction)
